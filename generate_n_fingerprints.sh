862c2b35a0ff3c2f39477653748e1ec4bc5c8138
Karine EM <karineven79@gmail.com>
2026-01-16 15:06:43 +0000

    Update script to use simPath for run_simshadow

--- generate_n_fingerprints.sh
+++ generate_n_fingerprints.sh
@@ -1,5 +1,7 @@
-n=$1 # how many fingerprints to generate for the statistical analysis later
+simPath=$1 # Abs. path to run_simshadow script (main entry)
+n=$2 # how many fingerprints to generate for the statistical analysis later
+
 for i in $(seq 1 $n); do
   echo ">> Executing run $i at $(date)"
-  SIMSHADOW_RUN_ID=$i python run_simshadow.py > logs/run_$i.log 2>&1
+  SIMSHADOW_RUN_ID=$i python $simPath/run_simshadow.py > logs/run_$i.log 2>&1
 done
