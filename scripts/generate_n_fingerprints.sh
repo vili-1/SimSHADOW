6903bac0d2d1bce0aa703053bc126309bc8fe6dc
Karine EM <karineven79@gmail.com>
2026-01-24 16:23:19 +0000

    Update generate_n_fingerprints.sh
    fix bug

--- scripts/generate_n_fingerprints.sh
+++ scripts/generate_n_fingerprints.sh
@@ -2,6 +2,7 @@ simPath=$1 # Abs. path to run_simshadow script (main entry)
 n=$2 # how many fingerprints to generate for the statistical analysis later
 
 mkdir $simPath/results/
+mkdir $simPath/logs/
 for i in $(seq 1 $n); do
   echo ">> Executing run $i at $(date)"
   SIMSHADOW_RUN_ID=$i python $simPath/run_simshadow.py > logs/run_$i.log 2>&1
